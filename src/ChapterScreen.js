931b654360b8ab49478afd2dee411fb2523d02c2
abayar <ahmet.bayar@hepsiburada.com>
2023-09-20 11:08:42 +0300

    test

--- src/ChapterScreen.js
+++ src/ChapterScreen.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import { View, StyleSheet,ScrollView } from 'react-native';
 import HTML from 'react-native-render-html';
+import { BannerAd, BannerAdSize, TestIds } from 'react-native-google-mobile-ads';
+const adUnitId = __DEV__ ? TestIds.BANNER : 'ca-app-pub-9981135393846597/1714353642';
 
 const ChapterScreen = ({ route }) => {
   const { chapterContent } = route.params;
 
   return (
     <View style={styles.container}>
+          <BannerAd
+      unitId={adUnitId}
+      size={BannerAdSize.ANCHORED_ADAPTIVE_BANNER}
+      requestOptions={{
+        requestNonPersonalizedAdsOnly: true,
+      }}
+    />
         <ScrollView>
         <HTML source={{ html: chapterContent }} />
 
